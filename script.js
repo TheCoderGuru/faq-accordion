2b882bf4f4b54b6221d2582f9808f52db067ddb3
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2022-03-06 20:19:50 -0400

    Updated the repository code

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-let title = document.querySelector('.down_arrow');
-let paragraph = document.querySelector('.description');
+const item = document.querySelectorAll(".item");
+const question = document.querySelectorAll(".question");
+const answer = document.querySelectorAll(".answer");
+const arrow = document.querySelectorAll(".arrow");
 
-title.addEventListener("click", () => {
-    paragraph.classList.add("show-bg");
-})
+item.forEach((ite, idx) => {
+  ite.addEventListener("click", () => {
+    removeActiveClasse();
+    removeShowClasse();
+    removeRotateClasse();
+
+    question[idx].classList.add("active");
+    answer[idx].classList.toggle("show");
+    arrow[idx].classList.toggle("rotate");
+  });
+});
+
+function removeShowClasse() {
+  answer.forEach((aswr) => {
+    aswr.classList.remove("show");
+  });
+}
+
+function removeActiveClasse() {
+  question.forEach((quest) => {
+    quest.classList.remove("active");
+  });
+}
+
+function removeRotateClasse() {
+  arrow.forEach((arr) => {
+    arr.classList.remove("rotate");
+  });
+}
